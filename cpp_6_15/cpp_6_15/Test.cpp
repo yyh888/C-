fcc04527a702a82c759c65a3b1e97ef6e95e5420
yyh888 <1161752748@qq.com>
2022-06-15 23:53:33 +0800

    cpp_6_15

---
+++ cpp_6_15/cpp_6_15/Test.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+
+using namespace std;
+
+
+//int Add(int a, int b)
+//{
+//	//int c = a + b;
+//	return a + b;
+//}
+//
+//int main()
+//{
+//	const int& rc = Add(1, 2);
+//	return 0;
+//}
+
+
+
+//int& Add(int a, int b)
+//{
+//	static int c = a + b;
+//	return c;
+//}
+//
+//int main()
+//{
+//	int& rc = Add(1, 2);
+//	Add(5, 7);
+//	cout << "hello" << endl;
+//	cout << rc << endl;
+//	return 0;
+//}
+
+
+

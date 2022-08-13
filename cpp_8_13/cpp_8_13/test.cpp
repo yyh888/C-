d0c3dfd046f7935b0abcd0f02d174e53026117f9
yyh888 <1161752748@qq.com>
2022-08-13 22:15:10 +0800

    cpp_8_13

---
+++ cpp_8_13/cpp_8_13/test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+//class A 
+//{
+//	friend class B;
+//public:
+//private:
+//	int _a;
+//};
+//
+////void Print(A& a)
+////{
+////	cout << a._a << endl;
+////}
+//
+//
+//class B
+//{
+//public:
+//	void Print(A& a)
+//	{
+//		cout << a._a << endl;
+//	}
+//private:
+//	A a;
+//};
+//
+//int main()
+//{
+//	B b;
+//	return 0;
+//}
+
+
+
